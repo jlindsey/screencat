be72806a38085401f5e096cb82bc77c0390b1076
Josh Lindsey <joshua.s.lindsey@gmail.com>
2018-03-27 10:51:01 -0400

    travis setup

---
+++ scripts/before_deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Build script shamelessly stolen from ripgrep :)
+
+cargo build --target $TARGET --release
+
+build_dir=$(mktemp -d 2>/dev/null || mktemp -d -t tmp)
+out_dir=$(pwd)
+name="${PROJECT_NAME}-${TRAVIS_TAG}-${TARGET}"
+mkdir "$build_dir/$name"
+
+cp target/$TARGET/release/$PROJECT_NAME "$build_dir/$name/"
+cp {LICENSE} "$build_dir/$name/"
+
+pushd $build_dir
+tar czf "$out_dir/$name.tar.gz" *
+popd
---
+++ scripts/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+cargo clean --target $TARGET --verbose
+cargo build --target $TARGET --verbose
+cargo test --target $TARGET --verbose
